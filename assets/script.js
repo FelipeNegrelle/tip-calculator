aaf130d5e9a45b7b849b104852e4a710f641bf4b
Felipe Negrelle <felipenegrelleramos@gmail.com>
2021-10-17 22:49:53 -0300

    18-10 commit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const bill = document.getElementById("bill");
+
+const tips = document.getElementById("btn");
+
+const tipCustom = document.getElementsByClassName("custom");
+
+const people = document.getElementById("people");
+
+const tipFinal = document.getElementsByClassName("tipAmount");
+
+const totalPerPerson = document.getElementsByClassName("totalPerson");
+
+const reset = document.getElementsByClassName("reset");
+
+
