fdef0c0b01e498835845872659ee2aee47de3d81
felipe negrelle ramos <felipenegrelleramos@gmail.com>
2021-10-23 23:01:35 -0300

    23-10 commit

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+"use strict";
+//input for the bill value 
+const bill = document.getElementById("bill");
+//button for the percentage of tip
+const tips = document.querySelectorAll("#btn");
+//button for custom percentage of tip
+const tipCustom = document.getElementsByClassName("custom");
+//input for the number of people to split the tip
+const people = document.getElementById("people");
+//place to showe the total of the bill
+const tipFinal = document.getElementsByClassName("tipAmount");
+//place to show the tip per person
+const totalPerPerson = document.getElementsByClassName("totalPerson");
+//button that reset the values
+const reset = document.getElementsByClassName("reset");
+//select percentage for calculate the bill
+const selected = document.querySelector("button.selected");
+
+tips.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("selected")) {
+            btn.classList.remove("selected");
+            btn.classList.add("tips-itens");
+            btn.classList.values();
+            console.log(tips);
+            console.log(selected);
+        } else {
+            tips.forEach(e => {
+                e.classList.remove("selected");
+            })
+            btn.classList.remove("tips-itens");
+            btn.classList.add("selected");
+            btn.classList.add("tips-itens");
+            console.log(tips);
+        }
+    })
+});
+
+function calculateBill() {
+    let billValue = parseInt(bill.value);
+    let percentage = parseInt(selected.value);
+    let peopleNum = parseInt(people.value);
+
+}
+calculateBill();
